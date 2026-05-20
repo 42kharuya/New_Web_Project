f8f5fad236c52057dfe90b78d3713aaaa2f0c18f
42kharuya <haruya.0411.k@gmail.com>
2026-05-21 04:03:59 +0900

    fix(monitoring): @sentry/cloudflareへ移行してWorkerバンドルサイズを削減する

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-const { withSentryConfig } = require("@sentry/nextjs");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-});
+module.exports = nextConfig;
